82e5fd6f99b66868be4ab3805233d7b223697fa8
NKolosov097 <N.Kolosov2003@mail.ru>
2023-07-14 22:57:07 +0300

    fix: sticky header, styles for body; add: custom scroll

--- src/components/technologies/technology/Technology.tsx
+++ src/components/technologies/technology/Technology.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import styles from "./Technology.module.css";
 
 interface ITechnologyProps {
@@ -7,8 +8,13 @@ interface ITechnologyProps {
 
 export const Technology = ({ icon, description }: ITechnologyProps) => {
   return (
-    <div className={styles.technology} title={description}>
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.8 }}
+      className={styles.technology}
+      title={description}
+    >
       {icon}
-    </div>
+    </motion.div>
   );
 };
